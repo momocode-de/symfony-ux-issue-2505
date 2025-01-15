9fc41768e98257437c6a54431f239c9d1405d0f6
Moritz Müller <moritz@momocode.de>
2025-01-15 10:46:52 +0100

    Add live component for test form

---
+++ src/Twig/Components/TestForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Form\TestType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+class TestForm extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(TestType::class);
+    }
+}
